83e15ad8e2dfb3e146be107baa0f2067ec647e74
Franklin <fabonero@ssct.edu.ph>
2023-03-29 13:11:15 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Clock from './Clock';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="App-content">
+                <Clock />
+            </div>
+        </div>
+    );
+}
+
+export default App;
